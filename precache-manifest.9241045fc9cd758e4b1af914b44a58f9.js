02202e1792fcf2ab35d6db2359b8767f601bf8c6
Luckyaremu <aremu.baba70@gmail.com>
2020-12-10 19:39:26 +0100

    Updates

---
+++ precache-manifest.9241045fc9cd758e4b1af914b44a58f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72caff2670097d9f55d0da1fb5a7aec",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "a30e37d894bbcd68b700",
+    "url": "/movies-app/static/css/main.ab75d1d4.chunk.css"
+  },
+  {
+    "revision": "3950d80fd3406caf0c38",
+    "url": "/movies-app/static/js/2.4bc9e548.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movies-app/static/js/2.4bc9e548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30e37d894bbcd68b700",
+    "url": "/movies-app/static/js/main.52380bf2.chunk.js"
+  },
+  {
+    "revision": "75ea8884925b9bad7552",
+    "url": "/movies-app/static/js/runtime-main.2bb9b983.js"
+  },
+  {
+    "revision": "b71b7d6046bdfbee99ed17e1c3adecdf",
+    "url": "/movies-app/static/media/spinner.b71b7d60.gif"
+  }
+]);
