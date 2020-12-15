61ce104249d5a807d21d2a774ea56cea082c4fa3
Luckyaremu <aremu.baba70@gmail.com>
2020-12-15 11:38:14 +0100

    test updated

--- src/components/home/SearchForm.js
+++ src/components/home/SearchForm.js
@@ -5,7 +5,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { SearchMovie, fetchMovies, setLoading } from '../../actions/SearchActions';
-import searchform from "../../styles/SearchForm.css"
+import searchform from "../../styles/SearchForm.css";
 
 export class SearchForm extends Component {
     onChange = e => {
